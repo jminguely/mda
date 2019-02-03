647eac1ae872ff17944e1fe9b8834738712cabdd
Julien Minguely <jminguely@gmail.com>
2019-02-03 13:28:48 -0500

    Adds an attempt for the mosaic

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -2,15 +2,19 @@
   <nav class="site-nav">
     <div class="row">
       <a class="nav-item site-title" href="/">La maison des artistes</a>
+      <span class="nav-separator"></span>
       <a class="nav-item" href="#">Projet</a>
     </div>
     <div class="row">
       <a class="nav-item" href="/">Locaux</a>
+      <span class="nav-separator"></span>
       <a class="nav-item" href="#">Coopérateurs.trices</a>
+      <span class="nav-separator"></span>
       <a class="nav-item" href="#">Contact</a>
     </div>
     <div class="row">
       <a class="nav-item" href="#">Partenaires</a>
+      <span class="nav-separator"></span>
       <div class="nav-item-group">
         <a class="nav-item" href="#">Facebook</a>
         <a class="nav-item" href="#">Instagram</a>
