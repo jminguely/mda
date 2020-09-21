8b5c7e51b4394499e9a2f58cb68f9e5b666b108e
jminguely <jminguely@gmail.com>
2020-09-21 11:10:12 +0200

    Typo change and new block

--- htdocs/content/themes/mda/config/cpt.php
+++ htdocs/content/themes/mda/config/cpt.php
@@ -3,14 +3,19 @@
 use Themosis\Support\Facades\Field;
 
 PostType::make('tenants', 'Locataires', 'Member')
-->setArguments([
+  ->setArguments([
     'public' => true,
     'menu_position' => 20,
-    'supports' => ['title', 'editor'],
+    'supports' => ['title', 'editor', 'thumbnail'],
     'rewrite' => false,
     'query_var' => false,
-    'menu_icon' => 'dashicons-smiley'
-])->set();
+    'menu_icon' => 'dashicons-smiley',
+    'show_in_rest' => false
+  ])->set();
+
+Metabox::make('tenants', 'tenants')
+  ->add(Field::text('url', ['title' => 'Lien de la page de l\'artiste'], ['class' => 'artist-link']))
+  ->set();
 
 Metabox::make('homepage', 'page')
   ->setTemplate('homepage')
